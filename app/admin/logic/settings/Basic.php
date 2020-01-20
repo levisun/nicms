f82561b45346ba1c9b066dbfa282dc2e10b572af
levisun <levisun.mail@icloud.com>
2020-01-20 22:37:38 +0800

    >_<

--- app/admin/logic/settings/Basic.php
+++ app/admin/logic/settings/Basic.php
@@ -74,7 +74,7 @@ class Basic extends BaseLogic
             'cms_footer'      => $this->request->param('cms_footer'),
             'cms_copyright'   => $this->request->param('cms_copyright', '', '\app\common\library\DataFilter::encode'),
             'cms_beian'       => $this->request->param('cms_beian'),
-            'cms_script'      => $this->request->param('cms_script', '', '\app\common\library\DataFilter::encode'),
+            'cms_script'      => $this->request->param('cms_script', '', 'strip_tags,trim,htmlspecialchars'),
         ];
         if ($result = $this->validate(__METHOD__, $receive_data)) {
             return $result;
